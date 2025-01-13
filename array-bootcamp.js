0d8c3e4a1d82b613297638ac7e3c55ffd430e389
lammai442 <lam.mai442@gmail.com>
2025-01-13 23:11:14 +0100

    Continued with array bootcamp. Done til 32

--- array-bootcamp.js
+++ array-bootcamp.js
@@ -4,7 +4,7 @@
 /* 1 */
 /* Gör en array som innehåller 5 st olika frukter av datatypen string. 
 
-Svar:
+SVAR:
 let fruits = ['Banan', 'Äpple', 'Kiwi', 'Melon', 'Apelsin']
 console.log(fruits);
 */
@@ -12,7 +12,7 @@ console.log(fruits);
 /* 2 */
 /* Gör en array som innehåller 4 olika datatyper, ex. string, number, array. 
 
-Svar:
+SVAR:
 let randomObjects = ['Banan', 3, true, ['Chiwawa', 'bulldog']];
 console.log(randomObjects);
 */
@@ -21,7 +21,7 @@ console.log(randomObjects);
 /* let animals = ['cat', 'hamster', 'parrot', 'funky chihuahua'] */
 /* console.log() hur många objekt arrayen ovan innehåller. 
 
-Svar:
+SVAR:
 let animals = ['cat', 'hamster', 'parrot', 'funky chihuahua'];
 console.log(`Hela arrayen innehåler ${animals.length} objekter och den andra har ${animals[1].length} tecken`);
 */
@@ -30,7 +30,7 @@ console.log(`Hela arrayen innehåler ${animals.length} objekter och den andra ha
 /* let animals = ['cat', 'hamster', 'parrot', 'funky chihuahua'] */
 /* I arrayen ovan, hämta cat. 
 
-Svar:
+SVAR:
 let animals = ['cat', 'hamster', 'parrot', 'funky chihuahua']
 console.log(animals[0]);
 */
@@ -39,7 +39,7 @@ console.log(animals[0]);
 /* let animals = ['cat', 'hamster', 'parrot', 'funky chihuahua'] */
 /* I arrayen ovan, hämta funky chihuahua. 
 
-Svar:
+SVAR:
 let animals = ['cat', 'hamster', 'parrot', 'funky chihuahua']
 console.log(animals[3]);
 //Alternativt med .at metoden
@@ -50,7 +50,7 @@ console.log(animals.at(-1));
 /* let animals = ['cat', 'hamster', 'parrot', 'funky chihuahua'] */
 /* I arrayen ovan, byt ut hamster mot tiger. 
 
-Svar:
+SVAR:
 let animals = ['cat', 'hamster', 'parrot', 'funky chihuahua']
 //Den första är på vilken indexplats som den ska ersätta [1], andra siffran [1] är den första nya objektet som ska in, vill man ha fler så lägger man in t ex [2] med 'dog'.
 animals.splice(1, 1, 'tiger', 2, 'dog',);
@@ -63,7 +63,7 @@ console.log(animals);
 /* let b = [4,5,6]; */
 /* Sätt ihop ovanstående arrayer. 
 
-Svar:
+SVAR:
 let a = [1,2,3];
 let b = [4, 5,6];
 //a.length anger vart den kommer börja nästa rad. 0 menas att inget ska tas bort eller ersättas, ...b är vad som ska läggas till.
@@ -77,7 +77,7 @@ console.log(a);
 /* let b = [4,5,6]; */
 /* Merga in array b på index 3 i array a.. 
 
-Svar:
+SVAR:
 let a = [1,2,3,7,8,9];
 let b = [4,5,6];
 
@@ -91,7 +91,7 @@ console.log(a);
 /* let arr = ['a', 'b', 'c']; */
 /* Klona ovanstående array. 
 
-Svar:
+SVAR:
 let arr = ['a', 'b', 'c'];
 let copyArr = [...arr];
 
@@ -102,7 +102,7 @@ console.log(copyArr);
 /* let fruits = ['kiwi', 'apple', 'pear']; */
 /* Lägg till en frukt i slutet av arrayen ovan. 
 
-Svar:
+SVAR:
 let fruits = ['kiwi', 'apple', 'pear'];
 fruits.push('banan')
 console.log(fruits);
@@ -113,7 +113,7 @@ console.log(fruits);
 /* let fruits = ['kiwi', 'apple', 'pear']; */
 /* Lägg till en frukt i början av arrayen ovan. 
 
-Svar:
+SVAR:
 let fruits = ['kiwi', 'apple', 'pear'];
 fruits.unshift('banan')
 
@@ -124,7 +124,7 @@ console.log(fruits);
 /* let fruits = ['kiwi', 'apple', 'pear']; */
 /* I arrayen ovan, ta bort sista frukten i arrayen. 
 
-Svar:
+SVAR:
 let fruits = ['kiwi', 'apple', 'pear'];
 fruits.pop()
 console.log(fruits);
@@ -134,7 +134,7 @@ console.log(fruits);
 /* let fruits = ['kiwi', 'apple', 'pear']; */
 /* I arrayen ovan, ta bort första frukten i arrayen. 
 
-Svar:
+SVAR:
 let fruits = ['kiwi', 'apple', 'pear'];
 fruits.shift();
 console.log(fruits);
@@ -144,7 +144,7 @@ console.log(fruits);
 /* let fruits = ['kiwi', 'apple', 'pear']; */
 /* Sätt in en frukt i arrayen ovan på index 1. 
 
-Svar:
+SVAR:
 let fruits = ['kiwi', 'apple', 'pear'];
 fruits.splice(1, 0, 'banan');
 console.log(fruits);
@@ -154,7 +154,7 @@ console.log(fruits);
 /* let fruits = ['kiwi', 'apple', 'pear']; */
 /* Sätt in, tre frukter i arrayen ovan på index 2. 
 
-Svar:
+SVAR:
 let fruits = ['kiwi', 'apple', 'pear'];
 fruits.splice(2, 0, 'banan', 'mango', 'apelsin');
 console.log(fruits);
@@ -164,7 +164,7 @@ console.log(fruits);
 /* let names = ['David', 'Christoffer', 'Johan', 'Maja'] */
 /* I arrayen ovan, ta bort Christoffer och Johan. 
 
-Svar:
+SVAR:
 let names = ['David', 'Christoffer', 'Johan', 'Maja'];
 names.splice(1, 2);
 console.log(names);
@@ -174,7 +174,7 @@ console.log(names);
 /* let nums = [1,2,3,4,5,6,7,8,9]; */
 /* Spegelvänd på arrayen ovan. 
 
-Svar:
+SVAR:
 let nums = [1,2,3,4,5,6,7,8,9];
 nums.reverse(); 
 console.log(nums);
@@ -184,116 +184,244 @@ console.log(nums);
 /* let str = 'Supercanlifragilisticexpialidocious'; */
 /* Kika om strängen ovan innehåller bokstaven n. 
 
-Svar:
+SVAR:
 let str = 'Supercalifragnilisticexpialidocious';
 console.log(`Finns det något n i ordet?: ${str.includes('n')}`);
 console.log(`Det finns på plats: ${str.indexOf('n')}`);
 */
 
 /* 19 */
-/* let str = 'Supercalifragilisticexpialidocious'; */
-/* Kika om strängen ovan innehåller bokstaven x. */
-console.log('Uppgift 19');
+/* let str = 'Supercalifragilisticexpialidocioxus'; */
+/* Kika om strängen ovan innehåller bokstaven x. 
+
+SVAR:
+let str = 'Supercalifragilisticexpialidocious';
+console.log(`${str.includes('x')}`);
+*/
 
 /* 20 */
 /* let str = 'Supercalifragilisticexpialidocious'; */
-/* I ovanstående sträng, hitta vilket position första förekomsten av p har. */
-console.log('Uppgift 20');
+/* I ovanstående sträng, hitta vilket position första förekomsten av p har.
 
+SVAR: 
+let str = 'Supercalifragilisticexpialidocious'; 
+console.log(`Den första förekomsten av 'p' kommer på plats ${str.indexOf('p')}`);
+*/
 
 /* 21 */
 /* let str = 'Supercalifragilisticexpialidocious'; */
-/* I strängen ovan, plocka fram de 5 första tecknena. */
-console.log('Uppgift 21');
+/* I strängen ovan, plocka fram de 5 första tecknena. 
 
+SVAR:
+let str = 'Supercalifragilisticexpialidocious'; 
+// SLICE-Metoden är mer flexibel är bättre
+console.log(`De första 5 tecknena är ${str.slice(0, 5)}`);
+console.log(`De första 5 tecknena är ${str.substring(0, 5)}`);
+*/
 
 /* 22 */
 /* let str = 'Supercalifragilisticexpialidocious'; */
-/* I strängen ovan, plocka fram de 7 sista tecknena. */
-console.log('Uppgift 22');
+/* I strängen ovan, plocka fram de 7 sista tecknena. 
+
+SVAR:
+let str = 'Supercalifragilisticexpialidocious';
+console.log(`De sista 7 tecknena är ${str.slice(-7)}`);
+*/
 
 //ADVANCED METHODS (HIGH ORDER METHODS)
+//.filter()
 /* 23 */
 /* let numArray = [23, 45, 5, 62, 1, 21, 3, 54]; */
-/* I arrayen ovan, filtera fram alla nummer över 5. Tips: Läs dokumentationen för filter() */
-console.log('Uppgift 23');
+/* I arrayen ovan, filtera fram alla nummer över 5. Tips: Läs dokumentationen för filter() 
 
+SVAR:
+let numArray = [23, 45, 5, 62, 1, 21, 3, 54];
+let aboveFive = numArray.filter((parameter) => parameter > 5);
+console.log(aboveFive);
+*/
 
 /* 24 */
 /* let numArray = [23, 45, 5, 62, 1, 21, 3, 54]; */
-/* I arrayen ovan, filtera fram alla nummer under 5. Tips: Läs dokumentationen för filter() */
-console.log('Uppgift 24');
+/* I arrayen ovan, filtera fram alla nummer under 5. Tips: Läs dokumentationen för filter() 
 
+SVAR:
+let numArray = [23, 45, 5, 62, 1, 21, 3, 54];
+let underFive = numArray.filter((parameter) => parameter < 5);
+console.log(underFive);
+*/
 
 /* 25 */
-/* Skriv ut alla namn som är 18 år eller över från arrayen längst ner i dokumentet. */
-console.log('Uppgift 25');
+/* Skriv ut alla namn som är 18 år eller över från arrayen. 
+let persons = [
+  {
+    name: "Felicia",
+    age: 12,
+  },
+  {
+    name: "Pelle",
+    age: 20,
+  },
+  {
+    name: "Peter",
+    age: 59,
+  },
+  {
+    name: "Anna",
+    age: 32,
+  },
+  {
+    name: "Jocke",
+    age: 18,
+  },
+  {
+    name: "Ella",
+    age: 3,
+  },
+];
+
+SVAR:
+let eigtheenOrAbove = persons.filter((parameter) => parameter.age > 18);
+console.log(eigtheenOrAbove);
+*/
 
 
 /* 26 */
-/* Skriv ut alla namn som är under 18 år från arrayen längst ner i dokumentet. */
-console.log('Uppgift 26');
-
-
-/* 27 */
-/* let arr = ['beta', 'alfa', 'gamma']; */
-/* Sortera ovanstående array alfabetisk. Tips: Läs dokumentattionen för sort() */
-console.log('Uppgift 27');
-
-
-/* 28 */
-/* let nums = [1,5,7,9,3,4,2,6,8]; */
-/* Sortera ovanstående array numeriskt. */
-console.log('Uppgift 28');
-
-
-//Array innehållandes flera objekt
+/* Skriv ut alla namn som är under 18 år.
 let persons = [
     {
-        name: "Felicia",
-        age: 12
+      name: "Felicia",
+      age: 12,
     },
     {
-        name: "Pelle",
-        age: 20
+      name: "Pelle",
+      age: 20,
     },
     {
-        name: "Peter",
-        age: 59
+      name: "Peter",
+      age: 59,
     },
     {
-        name: "Anna",
-        age: 32
+      name: "Anna",
+      age: 32,
     },
     {
-        name: "Jocke",
-        age: 18
+      name: "Jocke",
+      age: 18,
     },
     {
-        name: "Ella",
-        age: 3
-    }
-]
+      name: "Ella",
+      age: 3,
+    },
+  ];
+
+SVAR:
+let underEighteen = persons.filter((parameter) => parameter.age < 18)
+console.log(underEighteen);
+*/
+
+//.sort()
+/* 27 */
+/* let arr = ['beta', 'alfa', 'gamma']; */
+/* Sortera ovanstående array alfabetisk. Tips: Läs dokumentattionen för sort() 
+
+SVAR:
+let arr = ['beta', 'alfa', 'gamma'];
+console.log(arr.sort());
+*/
+
+/* 28 */
+/* let nums = [1,5,7,9,11,3,22,4,2,6,8]; */
+/* Sortera ovanstående array numeriskt. 
+
+SVAR:
+let nums = [1,5,7,9,11,3,22,4,2,6,8]; 
+// Måste lägga in denna funktion (a, b) => a - b för att de ska sortera siffor annars kommer allt över 10 hamna fel.
+console.log(nums.sort((a, b) => a - b));
+*/
 
 /* 29 */
-/* I person-arrayen längst ovan, sortera objekten efter ålder, yngst först. */
-console.log('Uppgift 29');
+/* I person-arrayen sortera objekten efter ålder, yngst först. 
 
+SVAR:
+let persons = [
+    {
+      name: "Felicia",
+      age: 12,
+    },
+    {
+      name: "Pelle",
+      age: 20,
+    },
+    {
+      name: "Peter",
+      age: 59,
+    },
+    {
+      name: "Anna",
+      age: 32,
+    },
+    {
+      name: "Jocke",
+      age: 18,
+    },
+    {
+      name: "Ella",
+      age: 3,
+    },
+  ];
+
+console.log(persons.sort((a, b) => a.age - b.age));
+*/
 
 /* 30 */
-/* I person-arrayen ovan, sortera objekten efter ålder, äldst först. */
-console.log('Uppgift 30');
+/* I person-arrayen ovan, sortera objekten efter ålder, äldst först. 
 
+SVAR:
+console.log(persons.sort((a, b) => b.age - a.age));
+*/
 
 /* 31 */
-/* I person-arrayen ovan, sortera objekten i bokstavsordning efter deras namn */
-console.log('Uppgift 31');
+/* I person-arrayen ovan, sortera objekten i bokstavsordning efter deras namn 
 
+SVAR:
+console.log(persons.sort((a, b) => a.name.localeCompare(b.name)));
+*/
 
+//.map()
 /* 32 */
-/* I person-arrayen ovan, gör om alla namn till versaler. */
-console.log('Uppgift 32');
+/* I person-arrayen ovan, gör om alla namn till versaler. 
 
+SVAR:
+let persons = [
+    {
+      name: "Felicia",
+      age: 12,
+    },
+    {
+      name: "Pelle",
+      age: 20,
+    },
+    {
+      name: "Peter",
+      age: 59,
+    },
+    {
+      name: "Anna",
+      age: 32,
+    },
+    {
+      name: "Jocke",
+      age: 18,
+    },
+    {
+      name: "Ella",
+      age: 3,
+    },
+  ];
+
+let nameUpperCase = persons.map((parameter) => parameter.name.toUpperCase());
+console.log(nameUpperCase);
+*/
 
 /* 33 */
 /* I person-arrayen ovan, spegelvänd alla namn. */
